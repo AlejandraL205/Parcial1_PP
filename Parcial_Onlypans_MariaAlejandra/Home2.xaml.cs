128d4a82c2131ef543dd8e84417a3e1164695760
AlejandraL205 <62623963+AlejandraL205@users.noreply.github.com>
2020-09-04 21:35:26 -0500

    Add files via upload

---
+++ Parcial_Onlypans_MariaAlejandra/Home2.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Parcial_Onlypans_MariaAlejandra
+{
+    /// <summary>
+    /// Lógica de interacción para Home2.xaml
+    /// </summary>
+    public partial class Home2 : Page
+    {
+        public Home2()
+        {
+            InitializeComponent();
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            string Nombre = txtNombre.Text;
+            string cedula = txtCedula.Text;
+            string Tipos = cbxTipodepanes.Text;
+            string Precios = lblPrecios.Content.ToString();
+            string Cantidad = srCantidadPanes.Value.ToString();
+            DateTime Now = DateTime.Now;
+
+            txtpedidos.Items.Add("Fecha: " + DateTime.Now + "\n" + "Nombre: " + Nombre + "\n" + "Cedula: " + cedula + "\n" + "Tipo: " + Tipos + "\n" + "Precio: " + Precios + "\n" + "Cantidad: " + Cantidad);
+
+
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+
+        {
+
+            RadioButton rb = sender as RadioButton;
+
+            if (rb.IsChecked.Value)
+
+            {
+
+                lblPrecios.Content = rb.Content.ToString();
+
+            }
+
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+
+        {
+
+            RadioButton rb = sender as RadioButton;
+
+            if (rb.IsChecked.Value)
+
+            {
+
+                lblPrecios.Content = rb.Content.ToString();
+
+            }
+
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+
+        {
+
+            RadioButton rb = sender as RadioButton;
+
+            if (rb.IsChecked.Value)
+
+            {
+
+                lblPrecios.Content = rb.Content.ToString();
+
+            }
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        {
+
+            ComboBox cb = sender as ComboBox;
+
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            MainWindow w = (MainWindow)Window.GetWindow(this);
+
+            w.mainFrame.NavigationService.Navigate(new Login());
+
+        }
+
+
+        private void srLevel_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+
+        {
+
+            lblCantidadPanes.Content = "Cantidad:" + srCantidadPanes.Value.ToString();
+
+        }
+    }
+}
---
+++ Parcial_Onlypans_MariaAlejandra/Login.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Parcial_Onlypans_MariaAlejandra
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            if (txtUsername.Text == "Administrador" && txtPassword.Password == "1234")
+
+            {
+                    MainWindow w = (MainWindow)Window.GetWindow(this);
+
+                    w.mainFrame.NavigationService.Navigate(new Home1());
+            }
+
+            else if (txtUsername.Text == "Otros" && txtPassword.Password == "1234")
+
+            {
+                    MainWindow w = (MainWindow)Window.GetWindow(this);
+
+                    w.mainFrame.NavigationService.Navigate(new Home2());
+            }
+
+            else
+
+            {
+                    MessageBox.Show("Invalid.");
+            }
+
+        }
+        
+    }
+}
